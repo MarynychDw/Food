db707f218790278903ec2b47e8958318e3f7e4b7
marynych.dw <marynych.dw@gmail.com>
2023-11-02 16:28:46 +0100

    updateModal

--- js/script.js
+++ js/script.js
@@ -111,17 +111,20 @@ window.addEventListener("DOMContentLoaded", () => {
 		modal = document.querySelector('.modal'),
 		modalCloseBtn = document.querySelector('[data-close]');
 
-	modalTrigger.forEach(btn => {
-		btn.addEventListener('click', () => {
-			modal.classList.toggle('show');
-			document.body.style.overflow = 'hidden';
-		});
-	});
+	function openModal () {
+		modal.classList.toggle('show');
+		document.body.style.overflow = 'hidden';
+		clearInterval(modalTimerId);
+	}
 
 	function closeModal() {
 		modal.classList.toggle('show');
 		document.body.style.overflow = '';
 	}
+
+	modalTrigger.forEach(btn => {
+		btn.addEventListener('click', openModal);
+	});
 
 	modalCloseBtn.addEventListener("click", closeModal);
 
@@ -136,4 +139,15 @@ window.addEventListener("DOMContentLoaded", () => {
 			closeModal();
 		}
 	});
+
+	const modalTimerId = setTimeout(openModal, 20000);
+
+	function showModalByScroll() {
+		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+			openModal();
+			window.removeEventListener('scroll', showModalByScroll);
+		}
+	}
+
+	window.addEventListener('scroll', showModalByScroll);
 });
